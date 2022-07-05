722ae714bbc431ddce42af70de4d388054dec6d4
thiago-ulloa-develcode <thiago.ulloa@develcode.com.br>
2022-07-05 10:57:48 -0300

    Criando a tabela

---
+++ tabela.js
@@ -0,0 +1,21 @@
+function filtrarNomes() {
+    // Declara variáveis
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("myTable");
+    tr = table.getElementsByTagName("tr");
+
+    // Cria um loop nas tabelas, e esconde aquelas que não correspodem a pesquisa
+    for (i = 0; i < tr.length; i++) {
+        td = tr[i].getElementsByTagName("td")[0];
+        if (td) {
+            txtValue = td.textContent || td.innerText;
+            if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                tr[i].style.display = "";
+            } else {
+             tr[i].style.display = "none";
+            }
+        }
+    }
+}
